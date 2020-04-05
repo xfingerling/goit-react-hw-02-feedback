a2a2ff3a5a7bf6ff8e76a1be429063a18388cbae
Oleh Zavhorodnii <oleggenius95@gmail.com>
2020-04-05 21:15:41 +0300

    Updates

---
+++ precache-manifest.f861d6dff3e2173483228c8c5162cc0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb6ba0bbdd96404dd6d9a58a93418db",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "4a516deef26f968a2c64",
+    "url": "/goit-react-hw-02-feedback/static/js/2.74f83195.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.74f83195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775cb66803cb97028206",
+    "url": "/goit-react-hw-02-feedback/static/js/main.bc39e121.chunk.js"
+  },
+  {
+    "revision": "5f76858bf903340caf72",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.50268c19.js"
+  }
+]);
